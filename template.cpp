6b84f1e0ef741500ad9fe8ebe0eee565313553c1
Subham503 <sahusubham38632@gmail.com>
2026-06-23 13:51:56 +0530

    Watermelon(4A)

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "Hello CP!\n";
+
+    return 0;
+}
